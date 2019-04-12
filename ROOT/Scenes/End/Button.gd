45832a3586a5c399a513d522fdeae812a2534551
afokin <andrii.fokin@ubisoft.com>
2019-04-13 01:16:24 +0300

    add end game menu

---
+++ ROOT/Scenes/End/Button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("button_up", self, "on_button_up")
+
+func _go_to_start_scene():
+	get_tree().change_scene("res://Scenes/Start/Start.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
