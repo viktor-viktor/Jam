034ef45bb99e827866badf6e02f4607dfd515118
yurii.smazhnyi <yurii.smazhnyi@ubisoft.com>
2019-04-12 21:35:08 +0300

    added start scene

---
+++ ROOT/Scenes/Start/Button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("button_up", self, "on_button_up")
+
+func on_button_up():
+	get_tree().change_scene("res://Root.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
