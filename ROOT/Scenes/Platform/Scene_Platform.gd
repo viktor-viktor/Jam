1de8f31b25bb937bb2374bcdc773c06674ccb053
Viktor Miroshnychenko <viktor.miroshnychenko@ubisoft.com>
2019-04-12 15:42:43 +0300

    Changed platform class

--- ROOT/Scenes/Platform/Scene_Platform.gd
+++ ROOT/Scenes/Platform/Scene_Platform.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-var gl_state = 2
+export(Root.WorldStates) var platform_type = Root.WorldStates.YELLOW
+
+func _ready():
+	change_world_state(platform_type)
 	
 func change_world_state(state):
 	
-	if gl_state == state:
+	if platform_type == state:
 		$CollisionShape2D_Platform.disabled = true
 		$Sprite_Platform.modulate.a8 = 128
 	
