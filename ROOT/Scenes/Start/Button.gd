0ba68a04f444e98390daa7d021d42eb16b326599
yurii.smazhnyi <yurii.smazhnyi@ubisoft.com>
2019-04-12 23:31:03 +0300

    start by button

--- ROOT/Scenes/Start/Button.gd
+++ ROOT/Scenes/Start/Button.gd
@@ -8,8 +8,15 @@ extends Button
 func _ready():
 	connect("button_up", self, "on_button_up")
 
-func on_button_up():
+func _start_scene():
 	get_tree().change_scene("res://Root.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		_start_scene()
+
+func on_button_up():
+	_start_scene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
